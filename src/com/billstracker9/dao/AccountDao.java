bae14bf379f07f0e9e520f5eb21b2a55f635c3d5
MohamedA <mohammedmon3m@yahoo.com>
2021-03-04 16:14:27 -0500

    Create AccountDao.java

---
+++ src/com/billstracker9/dao/AccountDao.java
@@ -0,0 +1,14 @@
+package com.billstracker9.dao;
+
+import java.util.List;
+
+import com.billstracker9.entities.Account;
+import com.billstracker9.entities.User;
+
+public interface AccountDao {
+	public int addAccount(Account account);
+	public int updateAccountBalance(String accountNumber,double balance);
+
+	public Account getAccountbyAccountNumber(String accountNumber);
+	public List<Account>getAccountListByUser(User user);
+}
