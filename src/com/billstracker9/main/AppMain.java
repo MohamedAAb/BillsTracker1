dcd5b08cb37294cab366da6918eb5caced15ceb3
MohamedA <mohammedmon3m@yahoo.com>
2021-03-04 16:17:59 -0500

    Create AppMain.java

---
+++ src/com/billstracker9/main/AppMain.java
@@ -0,0 +1,29 @@
+package com.billstracker9.main;
+
+import java.util.Calendar;
+
+import com.billstracker9.dao.AccountDaoImpl;
+import com.billstracker9.dao.PayeeDaoImpl;
+import com.billstracker9.dao.UserDaoImpl;
+import com.billstracker9.entities.Account;
+import com.billstracker9.entities.Payee;
+import com.billstracker9.entities.User;
+import com.billstracker9.services.UserServices;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//UserDaoImpl userDaoImpl=new UserDaoImpl();
+		//System.out.println("UpDate SessionId: "+userDaoImpl.updateUserSessionId("mohammedmon3m@yahoo.com", "111111111"));
+		//System.out.println("Right after UpDate SessionId: "+userDaoImpl.findByUserEmail("mohammedmon3m@yahoo.com"));
+		System.out.println("Calendar"+Calendar.getInstance().getTime());
+		
+		
+		
+		
+		
+
+	}
+
+}
