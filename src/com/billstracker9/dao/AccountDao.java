8caf304d2dfbba39c192e176e7d23d33fdd3f836
MohamedA <mohammedmon3m@yahoo.com>
2021-03-10 17:59:46 -0500

    Update AccountDao.java

--- src/com/billstracker9/dao/AccountDao.java
+++ src/com/billstracker9/dao/AccountDao.java
@@ -5,6 +5,10 @@ import java.util.List;
 import com.billstracker9.entities.Account;
 import com.billstracker9.entities.User;
 
+/**
+ * @author Mohamed Abouregila
+ *
+ */
 public interface AccountDao {
 	public int addAccount(Account account);
 	public int updateAccountBalance(String accountNumber,double balance);
